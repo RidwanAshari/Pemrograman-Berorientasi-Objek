5969633ee7f211ad7bf264751d961d412c059e41
Muhammad Ridwan Ashari <mridwanashari@gmail.com>
2024-09-01 19:30:11 +0700

    week 2

---
+++ Week 2/Main.java
@@ -0,0 +1,9 @@
+public class Main { 
+    public static void main(String[] args) { 
+        String[]items = {"Nama : Muhammad Ridwan Ashari","NPM : 2320506049","Alamat : Karangbener Rt 06 Rw 05, Bae, Kudus","No HP : 08989361842"};
+        
+        for (String item : items) {
+            System.out.println(item);
+        }  
+    }       
+}
